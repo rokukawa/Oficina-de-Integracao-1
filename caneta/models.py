739f8847704c8c50ed40cfc82fa4aeeb7991b1bd
Gustavo <gustavo_rokukawa@hotmail.com>
2021-09-27 16:29:44 -0300

    fix

--- caneta/models.py
+++ caneta/models.py
@@ -6,15 +6,15 @@ from datetime import datetime
 # Create your models here.
 
 class Caneta(models.Model):
-    modelo = models.CharField(max_length=45, null=False, blank=True)
-    cor = models.CharField(max_length=45, null=False, blank=True)
-    ponta = models.CharField(max_length=45, null=False, blank=True)
+    modelo = models.CharField(max_length=45, null=False)
+    cor = models.CharField(max_length=45, null=False)
+    ponta = models.CharField(max_length=45, null=False)
     
     def __str__(self):
         return self.modelo
 
 class Lote(models.Model):
-    codigo_maquina = models.CharField(max_length=45, null=False, blank=True)
+    codigo_maquina = models.CharField(max_length=45, null=False)
     data_fabricação = models.DateField(null=False)
     quantidade = models.IntegerField(null=False)
     caneta = models.ForeignKey(Caneta, on_delete=CASCADE)
